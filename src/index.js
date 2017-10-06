d77c05fd94f5e21c3f7e9edb1fa074e0db2d98e6
Emilija <emilija.tucovic@ezderm.com>
2017-10-06 11:44:50 +0200

    Todo App

--- src/index.js
+++ src/index.js
@@ -8,13 +8,12 @@
 // registerServiceWorker();
 
 
+/************************ For render to do list, render App component in the root *********************/
 import React from 'react';
-//import ReactDOM from 'react-dom';
 import { render } from 'react-dom'
 import {Provider} from 'react-redux'
 import './index.css';
-import App from './HOC/App';
-//import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
 import {createStore} from 'redux';
 import todoApp from './reducers';
 
@@ -22,11 +21,6 @@ import todoApp from './reducers';
 // Create store
 let store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-
-// ReactDOM.render(<Provider store = {store}>
-//         <App />
-//         </Provider>, document.getElementById('root'));
-// registerServiceWorker();
 render(
   <Provider store={store}>
     <App />
